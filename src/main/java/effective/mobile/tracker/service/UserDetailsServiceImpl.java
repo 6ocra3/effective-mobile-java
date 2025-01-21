5369dd3e3d2a82156b0d5e204a48e06035a5838d
6ocra3 <6o3kmm@mail.ru>
2025-01-21 20:18:06 +0300

    feat: add UserDetailsService

---
+++ src/main/java/effective/mobile/tracker/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package effective.mobile.tracker.service;
+
+import effective.mobile.tracker.models.User;
+import effective.mobile.tracker.repository.UserRepository;
+import effective.mobile.tracker.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found with name = " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
