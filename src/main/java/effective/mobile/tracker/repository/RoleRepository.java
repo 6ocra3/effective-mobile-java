e46fd4e317b96f65c1ac49c492c633f0e63786b2
6ocra3 <6o3kmm@mail.ru>
2025-01-21 20:01:20 +0300

    feat: add base role, user repository

---
+++ src/main/java/effective/mobile/tracker/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package effective.mobile.tracker.repository;
+
+import effective.mobile.tracker.models.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/effective/mobile/tracker/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package effective.mobile.tracker.repository;
+
+import effective.mobile.tracker.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
